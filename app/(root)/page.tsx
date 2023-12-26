7a4f3af0b1262edcd6063163b34d4a7575de0bfc
Gabry34 <gabrieleconciauro34@gmail.com>
2023-12-26 22:47:42 +0100

    commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   const [section, setSection] = useState("chat");
   const [rightSide, setRightSide] = useState("open");
   const router = useRouter();
-  const socket = io("http://localhost:5000");
+  const socket = io("http://localhost:8080");
 
   if (status !== "loading") {
     if (!session) {
