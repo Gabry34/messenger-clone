07d4ad057a1a9a13e42fda9d7e4301f02248d99e
Gabry34 <gabrieleconciauro34@gmail.com>
2023-12-27 16:38:15 +0100

    commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   const [section, setSection] = useState("chat");
   const [rightSide, setRightSide] = useState("open");
   const router = useRouter();
-  const socket = io("http://localhost:8080");
+  const socket = io("https://tranquil-beach-60657-dc67d0e54ddf.herokuapp.com/");
 
   if (status !== "loading") {
     if (!session) {
