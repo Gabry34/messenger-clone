59b9ceba582795f17f2dfc10ce86f5d6bb8ca3d8
Gabry34 <gabrieleconciauro34@gmail.com>
2023-12-27 16:55:41 +0100

    commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const io = require("socket.io")();
+// https://messenger-clone-peach-two.vercel.app
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("send-message", (number) => {
+    io.emit("refresh");
+    console.log(number);
+  });
+  socket.on("scroll-down", () => {
+    io.emit("scroll");
+  });
+  socket.on("get-user-data", () => {
+    // Creare una stanza unica per il socket corrente
+    const userRoom = socket.id;
+
+    // Unire il socket corrente alla stanza
+    socket.join(userRoom);
+
+    // Emettere l'evento solo alla stanza del socket corrente
+    io.to(userRoom).emit("get-data");
+  });
+  socket.on("open-preferences-modal", () => {
+    const userRoom = socket.id;
+
+    // Unire il socket corrente alla stanza
+    socket.join(userRoom);
+    console.log("ciao");
+    // Emettere l'evento solo alla stanza del socket corrente
+    io.to(userRoom).emit("open-modal");
+  });
+});
+
+io.listen(process.env.PORT || 8080);
