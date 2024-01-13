683d3d8bb8bc48f7aa2d04623dbdc2da5be685f2
Gabry34 <gabrieleconciauro34@gmail.com>
2024-01-13 10:30:05 +0100

    commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import logoImage from "@/public/images/logo.svg";
 import { FaFacebook } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { signIn, useSession } from "next-auth/react";
@@ -15,7 +14,7 @@ export default function LoginPage() {
   }
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center gap-10">
-      <img src={logoImage} alt="logo" width={80} height={80} />
+      <img src="/images/logo.svg" alt="logo" width={80} height={80} />
       <h1 className="text-black text-4xl text-center">
         Connect with the people you love.
       </h1>
